49e71666183702e1a70f7e819f38350366acd217
Daniel Yinanc <danielyinanc@users.noreply.github.com>
2016-11-13 22:10:30 -0500

    First Commit

---
+++ src/main/java/com/rainmakeross/cloud/registryuse/App.java
@@ -0,0 +1,45 @@
+package com.rainmakeross.cloud.registryuse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @RequestMapping("/getInfo")
+    public String serviceInstancesByApplicationName(
+            ) {
+        return restTemplate.getForObject("http://eureka-client/service-instances/{applicationName}",
+                String.class,"eureka-client");
+    }
+}
